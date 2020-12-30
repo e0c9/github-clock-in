2bb683bc8349f1c11a138a9a43262d161f58db35
Hao Tan <45457672+e0c9@users.noreply.github.com>
2020-12-30 16:54:13 +0800

    Update clock.py

--- clock.py
+++ clock.py
@@ -2,14 +2,12 @@ import requests
 from datetime import date
 
 response = requests.get('http://area.sinaapp.com/bingImg/')
-today = date.today()
-
-# dd/mm/YY
-today = today.strftime("%d/%m/%Y")
+now = datetime.now()
+now = now.strftime("%d/%m/%Y %H:%M:%S")
 
 with open('README.md', 'w') as f:
     f.write('# github clock in\n')
-    f.write('更新时间：{today}\n'.format(today=today))
+    f.write('更新时间：{now}\n'.format(now=now))
     content = """ <img style="-webkit-user-select: none;margin: auto;cursor: zoom-in;" src="{url}" width="1004" height="564"> """.format(url=response.url)
     print(content)
     f.write(content)
