65477b246c031c1c79a48e5b95bf6f307c9dab40
Tan Hao <tanhaoyc@gmail.com>
2020-12-30 16:42:39 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # github clock in
-![bing]('https://cn.bing.com/th?id=OHR.WinterBryce_ZH-CN8874624326_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&pid=hp')
+ <img style="-webkit-user-select: none;margin: auto;cursor: zoom-in;" src="https://cn.bing.com/th?id=OHR.WinterBryce_ZH-CN8874624326_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&pid=hp" width="1004" height="564"> 
--- clock.py
+++ clock.py
@@ -4,6 +4,6 @@ response = requests.get('http://area.sinaapp.com/bingImg/')
 
 with open('README.md', 'w') as f:
     f.write('# github clock in\n')
-    content = "![bing]('{url}')".format(url = response.url)
+    content = """ <img style="-webkit-user-select: none;margin: auto;cursor: zoom-in;" src="{url}" width="1004" height="564"> """.format(url=response.url)
     f.write(content)
 
