14ea67654397caa5ff62b0f7ca8a428fc8149a72
Hao Tan <45457672+e0c9@users.noreply.github.com>
2020-12-30 16:49:51 +0800

    Update clock.py

--- clock.py
+++ clock.py
@@ -5,5 +5,6 @@ response = requests.get('http://area.sinaapp.com/bingImg/')
 with open('README.md', 'w') as f:
     f.write('# github clock in\n')
     content = """ <img style="-webkit-user-select: none;margin: auto;cursor: zoom-in;" src="{url}" width="1004" height="564"> """.format(url=response.url)
+    print(content)
     f.write(content)
 
